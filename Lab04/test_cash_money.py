f1cec24b655bb9ae56b2f4b395eb39e6712c40b5
Dkeen <duncankeen@gmail.com>
2020-02-06 11:21:54 -0800

    sixth commit reformatted unittests

--- Lab04/test_cash_money.py
+++ Lab04/test_cash_money.py
@@ -4,6 +4,9 @@ from lab04 import cash_money
 
 class TestCashMoney(TestCase):
     def test_cash_money(self):
-        """Test cash_money function against the lower bound and a certain amount."""
+        """Test cash_money function against a certain amount."""
         self.assertEqual([2, 1, 0, 1, 0, 1, 1, 1, 0, 1, 2], cash_money(263.33))
+
+    def test_cash_money_lower_bound(self):
+        """Test cash_money function against the lower bound."""
         self.assertEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], cash_money(0))
--- Lab04/test_eratosthenes.py
+++ Lab04/test_eratosthenes.py
@@ -4,6 +4,9 @@ from lab04 import eratosthenes
 
 class TestEratosthenes(TestCase):
     def test_eratosthenes(self):
-        """Test eratosthenes against lower bound and a certain number."""
+        """Test eratosthenes against a certain number."""
         self.assertEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29], eratosthenes(30))
+
+    def test_eratosthenes_lower_bound(self):
+        """Test eratosthenes against lower bound."""
         self.assertEqual([], eratosthenes(0))
