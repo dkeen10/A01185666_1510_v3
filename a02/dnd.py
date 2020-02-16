043d0467c10516c52cd37ca0be7a095602f14b42
Dkeen <duncankeen@gmail.com>
2020-02-15 22:09:05 -0800

    finished combat_round unit test

--- a02/dnd.py
+++ a02/dnd.py
@@ -6,18 +6,18 @@ from random import randint
 def roll_die(number_of_rolls, number_of_sides):
     """Roll a die with the specified number of sides the specified number of times.
 
+    :param number_of_rolls: a positive non-zero integer
+    :param number_of_sides: a positive non-zero integer
+    :precondition: number_of_rolls and number_of_sides must be positive non-zero integers
+    :postcondition: generates a random dice role of the correct sided die being rolled the correct number of times
+    :return: a correctly generated sum of the dice roll as an integer
+
     Computational Thinking:
     Decomposition: Dice rolls  are used throughout this module, so making a roll_die helper function that can be invoked
         wherever needed reduces repeated code.
     Abstraction: This function can roll an any-positive-integer-sided die any positive-integer-number of times.
     Algorithms: I used a for loop to generate the dice roll since that correctly randomizes the result for each time
         the die is rolled.
-
-    :param number_of_rolls: a positive non-zero integer
-    :param number_of_sides: a positive non-zero integer
-    :precondition: number_of_rolls and number_of_sides must be positive non-zero integers
-    :postcondition: generates a random dice role of the correct sided die being rolled the correct number of times
-    :return: a correctly generated sum of the dice roll as an integer
     """
     dice_roll = 0
     for i in range(0, number_of_rolls):
@@ -28,13 +28,13 @@ def roll_die(number_of_rolls, number_of_sides):
 def generate_vowel():
     """Generate a random vowel.
 
+    :postcondition: a random vowel has been chosen from a list of vowels
+    :return: a random vowel
+
     Computational Thinking:
     Decomposition: This is a helper function to generate_syllable
     Abstraction: This same method of generating a list, then choosing a random element from the list is also used in
         generate_consonant, and can be used for get random choices for other applications.
-
-    :postcondition: a random vowel has been chosen from a list of vowels
-    :return: a random vowel
     """
     vowels = ["a", "e", "i", "o", "u", "y"]
     vowel = random.choice(vowels)
@@ -44,13 +44,13 @@ def generate_vowel():
 def generate_consonant():
     """Generate a random consonant.
 
+    :postcondition:a random consonant has been chosen from a list of consonants
+    :return: a random consonant
+
     Computational Thinking:
     Decomposition: This is a helper function to generate_syllable
     Abstraction: This same method of generating a list, then choosing a random element from the list is also used in
         generate_vowel, and can be used for get random choices for other applications.
-
-    :postcondition:a random consonant has been chosen from a list of consonants
-    :return: a random consonant
     """
     consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y",
                   "z"]
@@ -61,14 +61,14 @@ def generate_consonant():
 def generate_syllable():
     """Combine a random consonant and a random vowel into a consonant-vowel pair.
 
+    :postcondition: correctly generates a consonant-vowel pair of a random consonant and a random vowel
+    :return: a correctly generated consonant-vowel pair of a random consonant and a random vowel
+
     Computational Thinking:
     Decomposition: I used the generate syllable helper function to reduce repeated code. The generate syllable helper
         function then uses the generate_consonant and generate_vowel helper functions to generate a syllable.
     Algorithms: I used a for loop to populate an empty string with syllables consisting of consonant-vowel pairs up
         to the specified number of syllables.
-
-    :postcondition: correctly generates a consonant-vowel pair of a random consonant and a random vowel
-    :return: a correctly generated consonant-vowel pair of a random consonant and a random vowel
     """
     syllable = generate_consonant() + generate_vowel()
     return syllable
@@ -77,15 +77,15 @@ def generate_syllable():
 def generate_name(syllables):
     """Generate a random name.
 
-    Computational Thinking:
-    Decomposition: This function was functionally decomposed by using the helper function generate syllable.
-        In addition, this function is itself a helper function to create_character.
-    Algorithms: a for loop was used to populate the name with consonant-vowel pairs up to the number of syllables.
-
     :param syllables: a positive non-zero integer
     :precondition: syllables must be a positive non-zero integer
     :postcondition: generates a random name of the specified number of syllables
     :return: a randomly generated name of the specified number of syllables
+
+    Computational Thinking:
+    Decomposition: This function was functionally decomposed by using the helper function generate syllable.
+        In addition, this function is itself a helper function to create_character.
+    Algorithms: a for loop was used to populate the name with consonant-vowel pairs up to the number of syllables.
     """
     name = ""
     for i in range(int(syllables)):
@@ -96,6 +96,10 @@ def generate_name(syllables):
 def select_class():
     """Prompt user to select a class from a list.
 
+    :precondition: this function will only work if user selects a number from the list
+    :postcondition: the user has chosen a class
+    :return: the user's chosen class
+
     Computational Thinking:
     Decomposition: This is a helper function to the create_character function
     Abstraction: This enumerate method is also used in select_race and choose_inventory and is a good way to present
@@ -103,10 +107,6 @@ def select_class():
     Algorithms: The enumerate function was used to display the list of classes in a more user-friendly way, as well as
         allow for the user to make a selection by typing in the integer relating to the class instead of having to type
         in the whole class name.
-
-    :precondition: this function will only work if user selects a number from the list
-    :postcondition: the user has chosen a class
-    :return: the user's chosen class
     """
     roles = ["fighter", "paladin", "cleric", "monk", "barbarian", "rogue", "ranger", "bard", "druid", "wizard",
              "warlock", "sorcerer", ]
@@ -121,6 +121,10 @@ def select_class():
 def select_race():
     """Prompt user to select a race from a list.
 
+    :precondition: this function will only work if user selects a number from the list
+    :postcondition: the user has chosen a race
+    :return: the user's chosen race
+
     Computational Thinking:
     Decomposition: This is a helper function to the create_character function
     Abstraction: This enumerate method is also used in select_class and choose_inventory and is a good way to present
@@ -128,10 +132,6 @@ def select_race():
     Algorithms: The enumerate function was used to display the list of classes in a more user-friendly way, as well as
         allow for the user to make a selection by typing in the integer relating to the class instead of having to type
         in the whole class name.
-
-    :precondition: this function will only work if user selects a number from the list
-    :postcondition: the user has chosen a race
-    :return: the user's chosen race
     """
     races = ["human", "half-elf", "elf", "half-orc", "gnome", "halfling", "dwarf", "tiefling", "dragonborn"]
     print("\nRaces:")
@@ -145,6 +145,11 @@ def select_race():
 def create_character(syllables):
     """Create a character.
 
+    :param syllables: a positive non-zero integer
+    :precondition: syllables must be a positive non-zero integer, or anything else to get a helpful error message
+    :postcondition: a correctly created character in
+    :return: a correctly created character in, or None if syllables was in the incorrect form
+
     Computational Thinking:
     Decomposition: This populates an empty dictionary using several helper functions, including generate_name,
         select_class, select_race, and roll_die
@@ -153,11 +158,6 @@ def create_character(syllables):
     Abstraction: this function works for any input of syllables.  By using if statements, the function runs if syllables
         is an integer greater than zero, or prints an error message and returns None if syllables is zero or a negative
          integer, or even if it is not an integer at all.
-
-    :param syllables: a positive non-zero integer
-    :precondition: syllables must be a positive non-zero integer, or anything else to get a helpful error message
-    :postcondition: a correctly created character in
-    :return: a correctly created character in, or None if syllables was in the incorrect form
     """
     try:    # create_character first tries this set of code.
         if int(syllables) > 0:
@@ -205,14 +205,14 @@ def create_character(syllables):
 def print_dragon():
     r"""Print an ascii art dragon.
 
+    :postcondition: a correctly printed ascii art dragon
+
     Computational Thinking
     Decomposition: Originally, this function was in the main.  I created this helper function and evoked it in the main
         to make the main more readable while still keeping the lore.
     Pattern Matching: instead of doing prints statements for each each, I did one print statement with triple quotes.
         However, the backslashes in the ascii art were causing issues.  I then realized I could do a raw triple quote
         for the print statement as well as for the docstring itself so that the doctest would work.
-
-    :postcondition: a correctly printed ascii art dragon
 
     >>> print_dragon()
     <BLANKLINE>
@@ -259,12 +259,12 @@ def print_dragon():
 def print_character(character):
     """Print stylized text showing the user their created character.
 
-    Computational Thinking:
-    Algorithms: I used the .keys method to print the character information in a more user friendly way.
-
     :param character: a fully formed character in dictionary form
     :precondition: character must be a fully formed character in dictionary form
     :postcondition: a correctly printed and stylized version of the specified character
+
+    Computational Thinking:
+    Algorithms: I used the .keys method to print the character information in a more user friendly way.
 
     >>> print_character({'Name': 'ba', 'Class': 'barbarian', 'Race': 'human', 'Inventory': [], 'Experience': 0,\
                     'Strength': 14, 'Dexterity': 13, 'Constitution': 6, 'Intelligence': 8, 'Wisdom': 18, 'Charisma': 9,\
@@ -293,6 +293,11 @@ def print_character(character):
 def choose_inventory(character):
     """Prompt user to choose items for their character's inventory.
 
+    :param character: a fully formed character in dictionary form
+    :precondition: character must be a fully formed character in dictionary form.
+    :postcondition: user has chosen which items they wanted to buy, and those items have been added to the character's
+        inventory in the character dictionary.
+
     Computational Thinking:
     Abstraction: a while loop with nested if statements was used to allow for different scenarios.  First, if user
         input was within the range of the enumerated item list, then the function would run and the user would
@@ -303,11 +308,6 @@ def choose_inventory(character):
     Algorithms: The enumerate function was used to display the inventory list in a more user-friendly way, as well as
         allow for the user to make a selection by typing in the integer relating to the class instead of having to type
         in the whole class name.
-
-    :param character: a fully formed character in dictionary form
-    :precondition: character must be a fully formed character in dictionary form.
-    :postcondition: user has chosen which items they wanted to buy, and those items have been added to the character's
-        inventory in the character dictionary.
     """
     print("\nIf you want to beat the Dragonlord, first you need some gear. Visit Olgierd's shop.")
     print("\n\nWelcome to Olgierd's!\nHere's what I have for sale:")
@@ -330,12 +330,12 @@ def choose_inventory(character):
 def roll_for_initiative():
     """Roll for who attacks first in a round of combat.
 
+    :postcondition: Correctly returns True if initiative one is higher than initiative two, else False.
+    :return: True if initiative one is higher than initiative two, else False.
+
     Computational Thinking:
     Decomposition: The helper function roll_die was used to reduce repeated code.
     Algorithms: I used a while loop to repeat the function if the two initiative rolls are equal.
-
-    :postcondition: Correctly returns True if initiative one is higher than initiative two, else False.
-    :return: True if initiative one is higher than initiative two, else False.
     """
     initiative1 = 0
     initiative2 = 0
@@ -352,16 +352,16 @@ def roll_for_initiative():
 def roll_for_damage(character):
     """Roll to determine damage dealt.
 
-    Computational Thinking:
-    Decomposition: I invoked the roll_die helper function to reduce repeated code.
-    Pattern Matching: Since damage dealt is tied to a character's hit die, I grouped classes with the same hit die
-        together to reduce repeated code.
-
     :param character: a fully formed character in dictionary form
     :precondition: character must be a fully formed character in dictionary form
     :postcondition: returns a random integer value for damage dealt using the correct hit die, and a correctly printed
         message showing how much damage was dealt
     :return: a random integer value for damage dealt using the correct hit die
+
+    Computational Thinking:
+    Decomposition: I invoked the roll_die helper function to reduce repeated code.
+    Pattern Matching: Since damage dealt is tied to a character's hit die, I grouped classes with the same hit die
+        together to reduce repeated code.
     """
     character_for_damage = character
     if character_for_damage["Class"] in {"monk", "bard", "druid", "cleric", "rogue", "warlock"}:
@@ -377,13 +377,7 @@ def roll_for_damage(character):
 
 
 def roll_to_hit(opponent_one, opponent_two):
-    """Roll to hit against defender's dexterity.
-
-    Computational Thinking:
-    Decomposition: The roll_die helper function was used to reduce repeated code.
-    Pattern Matching:
-    Abstraction:
-    Algorithms:
+    """Roll to hit against opponent_two's dexterity.
 
     :param opponent_one: a fully formed character in dictionary form
     :param opponent_two: a fully formed character in dictionary form
@@ -391,6 +385,10 @@ def roll_to_hit(opponent_one, opponent_two):
     :postcondition: Correctly returns True if the character doing the hit has hit, else returns False if they missed.
         Both scenarios also include printing a relevant message.
     :return: True if the character doing the hit has hit, else returns False if they missed
+
+    Computational Thinking:
+    Decomposition: The roll_die helper function was used to reduce repeated code.  As well, this function itself is a
+        helper function to combat_round
     """
     character_doing_hit = opponent_one      # assigning parameters to local variables
     character_to_hit = opponent_two
@@ -407,6 +405,13 @@ def roll_to_hit(opponent_one, opponent_two):
 def combat_round(opponent_one, opponent_two):
     """Simulate one round of combat.
 
+    :param opponent_one: a fully formed character in dictionary form
+    :param opponent_two: a fully formed character in dictionary form
+    :precondition: both opponent_one and opponent_two must be fully formed characters in dictionary form.
+    :postcondition: Useful information about what occured during the combat round has been printed for the user, and if
+        the characters were dealt damagae during the combat round, that damage is returned and modifies their current HP
+    :return: each character's current HP
+
     Computational Thinking:
     Decomposition: The helper function roll_for_initiative was used to determine who the attacker and defender would be.
         Next the roll_to_hit helper function was used to determine if the attacker hit.  If the attacker did hit, the
@@ -422,13 +427,6 @@ def combat_round(opponent_one, opponent_two):
         can then sent to helper functions as either the attack or defender depending on who rolled higher initiative.
         It then uses nested if statements to play through a round of combat, allowing for hits, misses, lethal-damage,
         non-lethal damage, and retaliation.
-
-    :param opponent_one: a fully formed character in dictionary form
-    :param opponent_two: a fully formed character in dictionary form
-    :precondition: both opponent_one and opponent_two must be fully formed characters in dictionary form.
-    :postcondition: Useful information about what occured during the combat round has been printed for the user, and if
-        the characters were dealt damagae during the combat round, that damage is returned and modifies their current HP
-    :return: each character's current HP
     """
     if roll_for_initiative():
         attacker = opponent_one    # assigning the character that rolled higher in roll_for_initiative to the attacker
@@ -451,7 +449,7 @@ def combat_round(opponent_one, opponent_two):
             if attacker["HP"][1] <= 0:
                 print(f"{attacker['Name']} has died!")
 
-    return attacker["HP"][1], defender["HP"][1]
+    return attacker["HP"][1], defender["HP"][1]     # Returning attacker's and defender's current HP
 
 
 def main():
@@ -476,6 +474,7 @@ def main():
     print_character(villain)
     print("\nThe sounds of combat echo down the halls...\n")
     combat_round(main_character, villain)
+
 
     # to loop combat the following code code be used instead of the above line:
     # while True:
--- a02/test_combat_round.py
+++ a02/test_combat_round.py
@@ -5,36 +5,13 @@ import io
 from dnd import combat_round, roll_for_initiative, roll_to_hit, roll_for_damage
 
 
-# class TestCombatRound(TestCase):
-#     @patch("roll_for_damage", side_effect=[3])
-#     @patch("roll_to_hit", side_effect=[True, False])
-#     @patch("sys.stdout", new_callable=io.StringIO)
-#     @patch("roll_for_initiative", side_effect=[True])
-#     def test_combat_round_opponent_one_deals_damage_opponent_two_misses(self, mock_initiative, mock_stdout, mock_hit,
-#                                                                         mock_damage):
-#         opponent_one = {'Name': 'wyba', 'Class': 'fighter', 'Race': 'human', 'Inventory': [], 'Experience': 0,
-#                         'Strength': 14, 'Dexterity': 13, 'Constitution': 6, 'Intelligence': 8, 'Wisdom': 18,
-#                         'Charisma': 9, 'HP': [3, 3]}
-#
-#         opponent_two = {'Name': 'miraak', 'Class': 'barbarian', 'Race': 'dragonborn',
-#                 'Inventory': ["Miraak's Staff", "Miraak's Sword"], 'Experience': 100, 'Strength': 12, 'Dexterity': 10,
-#                 'constitution': 10, 'intelligence': 11, 'wisdom': 9, 'charisma': 8, 'HP': [7, 7]}
-#
-#         combat_round(opponent_one, opponent_two)
-#
-#         expected = """wyba goes first!
-#         wyba hits!
-#         wyba deals 3 damage!
-#         miraak misses!\n"""
-#         actual = mock_stdout.getvalue()
-#         self.assertEqual(actual, expected)
-
 class TestCombatRound(TestCase):
-    @patch("roll_for_damage", side_effect=[3])
-    @patch("roll_to_hit", side_effect=[True, False])
+    @patch("dnd.roll_for_damage", side_effect=[3])
+    @patch("dnd.roll_to_hit", side_effect=[True, False])
     @patch("sys.stdout", new_callable=io.StringIO)
-    @patch("roll_for_initiative", side_effect=[True])
-    def test_combat_round_changes_current_health(self):
+    @patch("dnd.roll_for_initiative", side_effect=[True])
+    def test_combat_round_opponent_one_hits_and_damages_opponent_two_misses(self, mock_initiative, mock_stdout,
+                                                                            mock_hit, mock_damage):
         opponent_one = {'Name': 'wyba', 'Class': 'fighter', 'Race': 'human', 'Inventory': [], 'Experience': 0,
                         'Strength': 14, 'Dexterity': 13, 'Constitution': 6, 'Intelligence': 8, 'Wisdom': 18,
                         'Charisma': 9, 'HP': [3, 3]}
@@ -43,7 +20,76 @@ class TestCombatRound(TestCase):
                 'constitution': 10, 'intelligence': 11, 'wisdom': 9, 'charisma': 8, 'HP': [7, 7]}
 
         combat_round(opponent_one, opponent_two)
-        expected = [7, 4]
-        actual = opponent_two["HP"]
+        expected_one = [3, 3]
+        actual_one = opponent_one['HP']
+        expected_two = [7, 4]
+        actual_two = opponent_two['HP']
 
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected_one, actual_one)
+        self.assertEqual(expected_two, actual_two)
+
+    @patch("dnd.roll_for_damage", side_effect=[3, 3])
+    @patch("dnd.roll_to_hit", side_effect=[True, True])
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("dnd.roll_for_initiative", side_effect=[True])
+    def test_combat_round_opponent_one_hits_and_damages_opponent_two_hits_and_damages(self, mock_initiative,
+                                                                                      mock_stdout, mock_hit,
+                                                                                      mock_damage):
+        opponent_one = {'Name': 'wyba', 'Class': 'fighter', 'Race': 'human', 'Inventory': [], 'Experience': 0,
+                        'Strength': 14, 'Dexterity': 13, 'Constitution': 6, 'Intelligence': 8, 'Wisdom': 18,
+                        'Charisma': 9, 'HP': [3, 3]}
+        opponent_two = {'Name': 'miraak', 'Class': 'barbarian', 'Race': 'dragonborn',
+                'Inventory': ["Miraak's Staff", "Miraak's Sword"], 'Experience': 100, 'Strength': 12, 'Dexterity': 10,
+                'constitution': 10, 'intelligence': 11, 'wisdom': 9, 'charisma': 8, 'HP': [7, 7]}
+
+        combat_round(opponent_one, opponent_two)
+        expected_one = [3, 0]
+        actual_one = opponent_one['HP']
+        expected_two = [7, 4]
+        actual_two = opponent_two['HP']
+
+        self.assertEqual(expected_one, actual_one)
+        self.assertEqual(expected_two, actual_two)
+
+    @patch("dnd.roll_for_damage", side_effect=[3])
+    @patch("dnd.roll_to_hit", side_effect=[True])
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("dnd.roll_for_initiative", side_effect=[False])
+    def test_combat_round_opponent_one_misses_opponent_two_hits_and_damages(self, mock_initiative,
+                                                                                      mock_stdout, mock_hit,
+                                                                                      mock_damage):
+        opponent_one = {'Name': 'wyba', 'Class': 'fighter', 'Race': 'human', 'Inventory': [], 'Experience': 0,
+                        'Strength': 14, 'Dexterity': 13, 'Constitution': 6, 'Intelligence': 8, 'Wisdom': 18,
+                        'Charisma': 9, 'HP': [3, 3]}
+        opponent_two = {'Name': 'miraak', 'Class': 'barbarian', 'Race': 'dragonborn',
+                'Inventory': ["Miraak's Staff", "Miraak's Sword"], 'Experience': 100, 'Strength': 12, 'Dexterity': 10,
+                'constitution': 10, 'intelligence': 11, 'wisdom': 9, 'charisma': 8, 'HP': [7, 7]}
+
+        combat_round(opponent_one, opponent_two)
+        expected_one = [3, 0]
+        actual_one = opponent_one['HP']
+        expected_two = [7, 7]
+        actual_two = opponent_two['HP']
+
+        self.assertEqual(expected_one, actual_one)
+        self.assertEqual(expected_two, actual_two)
+
+    @patch("dnd.roll_to_hit", side_effect=[False, False])
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("dnd.roll_for_initiative", side_effect=[False])
+    def test_combat_round_opponent_one_misses_opponent_two_misses(self, mock_initiative, mock_stdout, mock_hit):
+        opponent_one = {'Name': 'wyba', 'Class': 'fighter', 'Race': 'human', 'Inventory': [], 'Experience': 0,
+                        'Strength': 14, 'Dexterity': 13, 'Constitution': 6, 'Intelligence': 8, 'Wisdom': 18,
+                        'Charisma': 9, 'HP': [3, 3]}
+        opponent_two = {'Name': 'miraak', 'Class': 'barbarian', 'Race': 'dragonborn',
+                'Inventory': ["Miraak's Staff", "Miraak's Sword"], 'Experience': 100, 'Strength': 12, 'Dexterity': 10,
+                'constitution': 10, 'intelligence': 11, 'wisdom': 9, 'charisma': 8, 'HP': [7, 7]}
+
+        combat_round(opponent_one, opponent_two)
+        expected_one = [3, 3]
+        actual_one = opponent_one['HP']
+        expected_two = [7, 7]
+        actual_two = opponent_two['HP']
+
+        self.assertEqual(expected_one, actual_one)
+        self.assertEqual(expected_two, actual_two)
