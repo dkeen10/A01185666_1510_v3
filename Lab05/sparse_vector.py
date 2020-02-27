05f634b6694702e0fa5f31e2e0eb5f887e1dfa63
Dkeen <duncankeen@gmail.com>
2020-02-27 00:11:17 -0800

    added more annotations and doctests

--- Lab05/sparse_vector.py
+++ Lab05/sparse_vector.py
@@ -1,17 +1,23 @@
 import doctest
+from typing import Union
 
 
-def sparse_add(vector_one: dict, vector_two: dict):
+def sparse_add(vector_one: dict, vector_two: dict) -> Union[dict, None]:
     """Add two sparse vectors.
 
-    :precon: well-formed dictionary
-
+    :param vector_one: a sparse vector in dictionary form
+    :param vector_two: a sparse vector in dictionary form
+    :precondition: vector_one and vector two must be two well-formed sparse vectors in dictionary form
+    :postcondition: calculates the correct sum of the two specified sparse vectors
+    :return: the correct sum of the two specified sparse vectors
     >>> sparse_add({'length': 5, 2: 7.5, 4: -6}, {4: -5, 'length': 5, 0: 4.3})
     {'length': 5, 2: 7.5, 4: -11, 0: 4.3}
     >>> sparse_add({'length': 10, 7:-1}, {'length': 10, 7: 1})
     {'length': 10}
+    >>> sparse_add({'length':5}, {'length':7})
+
     """
-    if vector_one['length'] != vector_two['length']:    # first line of code should check that parameter are the same length.
+    if vector_one['length'] != vector_two['length']:
         return None
 
     else:
@@ -28,20 +34,24 @@ def sparse_add(vector_one: dict, vector_two: dict):
     return combined_vector
 
 
-def sparse_dot_product(vector_one, vector_two):
+def sparse_dot_product(vector_one: dict, vector_two: dict) -> Union[int, float, None]:
     """Calculate dot product of the two provided sparse vectors.
 
-    :precondition: vector_one and vector two must be two well-formed sparse vectors in dictionary form.
-    :postcondition:
-    :return: the correctly calculated dot product
+    :param vector_one: a sparse vector in dictionary form
+    :param vector_two: a sparse vector in dictionary form
+    :precondition: vector_one and vector two must be two well-formed sparse vectors in dictionary form
+    :postcondition: correctly calculates the dot product of the two specified sparse vectors
+    :return: the correctly calculated dot product of the two specified sparse vectors
     >>> sparse_dot_product({'length': 5, 2: 7.5, 4: -6}, {4: -5, 'length': 5, 0: 4.3})
     30
     >>> sparse_dot_product({'length':0}, {'length':0})
     0
     >>> sparse_dot_product({'length':5}, {'length':7})
 
+    >>> sparse_dot_product({4: -1, 2: 1.5, 'length':5}, {2: 3, 'length':5, 1: 5})
+    4.5
     """
-    if vector_one['length'] != vector_two['length']:    # first line of code should check that parameter are the same length.
+    if vector_one['length'] != vector_two['length']:
         return None
 
     else:
