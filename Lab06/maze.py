0f701941d3e5735bf6b4622f1362a94767306822
Dkeen <duncankeen@gmail.com>
2020-03-03 13:35:21 -0800

    Am now using the board.  However, it is much more complicated and worked fine before.

--- Lab06/maze.py
+++ Lab06/maze.py
@@ -8,32 +8,66 @@
 
 
 def make_board():
-    board = {(0, 0): [0, 0], (0, 1): [0, 1], (0, 2): [0, 2], (0, 3): [0, 3], (0, 4): [0, 4],
-             (1, 0): [1, 0], (1, 1): [1, 1], (1, 2): [1, 2], (1, 3): [1, 3], (1, 4): [1, 4],
-             (2, 0): [2, 0], (2, 1): [2, 1], (2, 2): [2, 2], (2, 3): [2, 3], (2, 4): [2, 4],
-             (3, 0): [3, 0], (3, 1): [3, 1], (3, 2): [3, 2], (3, 3): [3, 3], (3, 4): [3, 4],
-             (4, 0): [4, 0], (4, 1): [4, 1], (4, 2): [4, 2], (4, 3): [4, 3], (4, 4): [4, 4]}
+    # board = {(0, 0): [0, 0], (0, 1): [0, 1], (0, 2): [0, 2], (0, 3): [0, 3], (0, 4): [0, 4],
+    #          (1, 0): [1, 0], (1, 1): [1, 1], (1, 2): [1, 2], (1, 3): [1, 3], (1, 4): [1, 4],
+    #          (2, 0): [2, 0], (2, 1): [2, 1], (2, 2): [2, 2], (2, 3): [2, 3], (2, 4): [2, 4],
+    #          (3, 0): [3, 0], (3, 1): [3, 1], (3, 2): [3, 2], (3, 3): [3, 3], (3, 4): [3, 4],
+    #          (4, 0): [4, 0], (4, 1): [4, 1], (4, 2): [4, 2], (4, 3): [4, 3], (4, 4): [4, 4]}
+    coordinates = [(i, j) for j in range(5) for i in range(5)]
+    k = 0
+    board = {}
+    while k < len(coordinates):
+        board[coordinates[k]] = False
+        k += 1
+    board[(4, 4)] = True
+    print(board)
     return board
 
 
-def validate_move(character, direction):
+# def make_board():
+#     board = [
+#             [1, 1]
+#     ]
+
+
+def validate_move(board, character, direction):
     if direction not in {"N", "E", "S", "W"}:
         print("Direction must be N, E, S, or W:")
         return False
-    if direction == "N" and character["Location"][0] == 0:
+    if direction == "N" and (tuple(map(sum, zip(character["Location"], (-1, 0)))) not in board.keys()):
         print("You are at the northern edge of the maze! Enter a different direction:")
         return False
-    if direction == "S" and character["Location"][0] == 4:
+    if direction == "S" and (tuple(map(sum, zip(character["Location"], (1, 0)))) not in board.keys()):
         print("You are at the southern edge of the maze! Enter a different direction:")
         return False
-    if direction == "E" and character["Location"][1] == 4:
+    if direction == "E" and (tuple(map(sum, zip(character["Location"], (0, 1)))) not in board.keys()):
         print("You are at the eastern edge of the maze! Enter a different direction:")
         return False
-    if direction == "W" and character["Location"][1] == 0:
+    if direction == "W" and (tuple(map(sum, zip(character["Location"], (0, -1)))) not in board.keys()):
         print("You are at the western edge of the maze! Enter a different direction:")
         return False
     else:
         return True
+
+
+# def validate_move(character, direction):
+#     if direction not in {"N", "E", "S", "W"}:
+#         print("Direction must be N, E, S, or W:")
+#         return False
+#     if direction == "N" and character["Location"][0] == 0:
+#         print("You are at the northern edge of the maze! Enter a different direction:")
+#         return False
+#     if direction == "S" and character["Location"][0] == 4:
+#         print("You are at the southern edge of the maze! Enter a different direction:")
+#         return False
+#     if direction == "E" and character["Location"][1] == 4:
+#         print("You are at the eastern edge of the maze! Enter a different direction:")
+#         return False
+#     if direction == "W" and character["Location"][1] == 0:
+#         print("You are at the western edge of the maze! Enter a different direction:")
+#         return False
+#     else:
+#         return True
 
 
 def move_character(direction, character):
@@ -60,13 +94,13 @@ def move_character(direction, character):
 
 
 def game():
-    # board = make_board()
+    board = make_board()
     character = {"Location": [0, 0]}
     reached_goal = False
     while not reached_goal:
         print(f"Current Location: {character['Location']}")
         direction = input("Move N, S, E, or W?")
-        valid_move = validate_move(character, direction)
+        valid_move = validate_move(board, character, direction)
         if valid_move:
             move_character(direction, character)
             if character["Location"] == [4, 4]:
