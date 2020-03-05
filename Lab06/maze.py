bde8a7b025e551150289c031b2f473601f4e5f4b
Dkeen <duncankeen@gmail.com>
2020-03-05 15:13:10 -0800

    finished quiz 06

--- Lab06/maze.py
+++ Lab06/maze.py
@@ -1,42 +1,31 @@
 import doctest
-# def make_board():
-#     board = {(0, 0): 1, (0, 1): 2, (0, 2): 3, (0, 3): 4, (0, 4): 5,
-#              (1, 0): 6, (1, 1): 7, (1, 2): 8, (1, 3): 9, (1, 4): 10,
-#              (2, 0): 11, (2, 1): 12, (2, 2): 13, (2, 3): 14, (2, 4): 15,
-#              (3, 0): 16, (3, 1): 17, (3, 2): 18, (3, 3): 19, (3, 4): 20,
-#              (4, 0): 21, (4, 1): 22, (4, 2): 23, (4, 3): 24, (4, 4): 25}
-#     return board
 
 
-def make_board():
+def make_board(size: int) -> dict:
     """Create the maze board.
 
     :postcondition: a dictionary containing a 5x5 coordinate maze has been successfully created
     :return: a dictionary containing a 5x5 coordinate maze
 
-    >>> make_board()
+    >>> test_size = 5
+    >>> make_board(test_size)
     {(0, 0): False, (1, 0): False, (2, 0): False, (3, 0): False, (4, 0): False, (0, 1): False, (1, 1): False, \
 (2, 1): False, (3, 1): False, (4, 1): False, (0, 2): False, (1, 2): False, (2, 2): False, (3, 2): False, \
 (4, 2): False, (0, 3): False, (1, 3): False, (2, 3): False, (3, 3): False, (4, 3): False, (0, 4): False, \
 (1, 4): False, (2, 4): False, (3, 4): False, (4, 4): True}
 
     """
-    # board = {(0, 0): [0, 0], (0, 1): [0, 1], (0, 2): [0, 2], (0, 3): [0, 3], (0, 4): [0, 4],
-    #          (1, 0): [1, 0], (1, 1): [1, 1], (1, 2): [1, 2], (1, 3): [1, 3], (1, 4): [1, 4],
-    #          (2, 0): [2, 0], (2, 1): [2, 1], (2, 2): [2, 2], (2, 3): [2, 3], (2, 4): [2, 4],
-    #          (3, 0): [3, 0], (3, 1): [3, 1], (3, 2): [3, 2], (3, 3): [3, 3], (3, 4): [3, 4],
-    #          (4, 0): [4, 0], (4, 1): [4, 1], (4, 2): [4, 2], (4, 3): [4, 3], (4, 4): [4, 4]}
-    coordinates = [(i, j) for j in range(5) for i in range(5)]
+    coordinates = [(i, j) for j in range(size) for i in range(size)]
     k = 0
     board = {}
     while k < len(coordinates):
         board[coordinates[k]] = False
         k += 1
-    board[(4, 4)] = True
+    board[(size - 1, size - 1)] = True
     return board
 
 
-def validate_move(board, character, direction):
+def validate_move(board: dict, character: dict, direction: str) -> bool:
     """Check if direction is valid
 
     :param board: a 5x5 coordinate dictionary
@@ -55,6 +44,46 @@ def validate_move(board, character, direction):
     >>> validate_move(test_board, test_character, test_direction1)
     Direction must be N, E, S, or W.
     False
+
+    >>> test_direction1 = "N"
+    >>> test_board = {(0, 0): False, (1, 0): False, (2, 0): False, (3, 0): False, (4, 0): False, (0, 1): False, \
+(1, 1): False, (2, 1): False, (3, 1): False, (4, 1): False, (0, 2): False, (1, 2): False, (2, 2): False, \
+(3, 2): False, (4, 2): False, (0, 3): False, (1, 3): False, (2, 3): False, (3, 3): False, (4, 3): False, \
+(0, 4): False, (1, 4): False, (2, 4): False, (3, 4): False, (4, 4): True}
+    >>> test_character = {"Location": [0,0]}
+    >>> validate_move(test_board, test_character, test_direction1)
+    You are at the northern edge of the maze! Enter a different direction.
+    False
+
+    >>> test_direction1 = "S"
+    >>> test_board = {(0, 0): False, (1, 0): False, (2, 0): False, (3, 0): False, (4, 0): False, (0, 1): False, \
+(1, 1): False, (2, 1): False, (3, 1): False, (4, 1): False, (0, 2): False, (1, 2): False, (2, 2): False, \
+(3, 2): False, (4, 2): False, (0, 3): False, (1, 3): False, (2, 3): False, (3, 3): False, (4, 3): False, \
+(0, 4): False, (1, 4): False, (2, 4): False, (3, 4): False, (4, 4): True}
+    >>> test_character = {"Location": [4,0]}
+    >>> validate_move(test_board, test_character, test_direction1)
+    You are at the southern edge of the maze! Enter a different direction.
+    False
+
+    >>> test_direction1 = "E"
+    >>> test_board = {(0, 0): False, (1, 0): False, (2, 0): False, (3, 0): False, (4, 0): False, (0, 1): False, \
+(1, 1): False, (2, 1): False, (3, 1): False, (4, 1): False, (0, 2): False, (1, 2): False, (2, 2): False, \
+(3, 2): False, (4, 2): False, (0, 3): False, (1, 3): False, (2, 3): False, (3, 3): False, (4, 3): False, \
+(0, 4): False, (1, 4): False, (2, 4): False, (3, 4): False, (4, 4): True}
+    >>> test_character = {"Location": [0,4]}
+    >>> validate_move(test_board, test_character, test_direction1)
+    You are at the eastern edge of the maze! Enter a different direction.
+    False
+
+    >>> test_direction1 = "W"
+    >>> test_board = {(0, 0): False, (1, 0): False, (2, 0): False, (3, 0): False, (4, 0): False, (0, 1): False, \
+(1, 1): False, (2, 1): False, (3, 1): False, (4, 1): False, (0, 2): False, (1, 2): False, (2, 2): False, \
+(3, 2): False, (4, 2): False, (0, 3): False, (1, 3): False, (2, 3): False, (3, 3): False, (4, 3): False, \
+(0, 4): False, (1, 4): False, (2, 4): False, (3, 4): False, (4, 4): True}
+    >>> test_character = {"Location": [0,0]}
+    >>> validate_move(test_board, test_character, test_direction1)
+    You are at the western edge of the maze! Enter a different direction.
+    False
     """
     if direction not in {"N", "E", "S", "W"}:
         print("Direction must be N, E, S, or W.")
@@ -69,33 +98,13 @@ def validate_move(board, character, direction):
         print("You are at the eastern edge of the maze! Enter a different direction.")
         return False
     if direction == "W" and (tuple(map(sum, zip(character["Location"], (0, -1)))) not in board.keys()):
-        print("You are at the western edge of the maze! Enter a different direction:")
+        print("You are at the western edge of the maze! Enter a different direction.")
         return False
     else:
         return True
 
 
-# def validate_move(character, direction):
-#     if direction not in {"N", "E", "S", "W"}:
-#         print("Direction must be N, E, S, or W:")
-#         return False
-#     if direction == "N" and character["Location"][0] == 0:
-#         print("You are at the northern edge of the maze! Enter a different direction:")
-#         return False
-#     if direction == "S" and character["Location"][0] == 4:
-#         print("You are at the southern edge of the maze! Enter a different direction:")
-#         return False
-#     if direction == "E" and character["Location"][1] == 4:
-#         print("You are at the eastern edge of the maze! Enter a different direction:")
-#         return False
-#     if direction == "W" and character["Location"][1] == 0:
-#         print("You are at the western edge of the maze! Enter a different direction:")
-#         return False
-#     else:
-#         return True
-
-
-def move_character(direction, character):
+def move_character(direction: str, character: dict):
     """Move the character.
 
     :param direction: a direction of either N, S, E, or W
@@ -137,24 +146,13 @@ def move_character(direction, character):
         character["Location"][1] += 1
 
 
-# def move_character(board, direction, character):
-#     for key in board.keys():
-#         if direction == "N":
-#             character["Location"] = board[key][0] + 1
-#         if direction == "S":
-#             character["Location"][0] += 1
-#         if direction == "W":
-#             character["Location"][1] -= 1
-#         if direction == "E":
-#             character["Location"][1] += 1
-
-
 def game():
     """Play the maze game.
 
     :postcondition: user has successfully played the maze game
     """
-    board = make_board()
+    size = 5
+    board = make_board(size)
     character = {"Location": [0, 0]}
     reached_goal = False
     while not reached_goal:
@@ -164,6 +162,7 @@ def game():
         if valid_move:
             move_character(direction, character)
             if board[tuple(character["Location"])]:
+                print(f"Current Location: {character['Location']}")
                 print("Congratulations! You've reached the end of the maze!")
                 reached_goal = True
 
--- Lab06/test_validate_move.py
+++ Lab06/test_validate_move.py
@@ -6,55 +6,56 @@ from maze import validate_move, make_board
 
 
 class TestValidateMove(TestCase):
-    @patch('maze.game', side_effect=["tuba"])
-    @patch('maze.make_board')
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_validate_move_not_in_NSEW(self, mock_stdout, board, direction):
-        test_character = {"Location": [0, 0]}
-        actual = validate_move(board, test_character, direction)
+    def test_validate_move_not_in_NSEW(self, mock_stdout):
+        mock_board = make_board()
+        mock_character = {"Location": [2, 2]}
+        mock_direction = "tuba"
+        actual = validate_move(mock_board, mock_character, mock_direction)
         expected = False
         self.assertEqual(expected, actual)
 
-    @patch('maze.game', side_effect=["N"])
-    @patch('maze.make_board')
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_validate_move_north_at_northern_edge(self, mock_stdout, board, direction):
-        test_character = {"Location": [0, 0]}
-        actual = validate_move(board, test_character, direction)
+    def test_validate_move_north_at_northern_edge(self, mock_stdout):
+        mock_board = make_board()
+        mock_character = {"Location": [0, 0]}
+        mock_direction = "N"
+        actual = validate_move(mock_board, mock_character, mock_direction)
         expected = False
         self.assertEqual(expected, actual)
 
-    @patch('maze.game', side_effect=["S"])
-    @patch('maze.make_board')
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_validate_move_north_at_northern_edge(self, mock_stdout, board, direction):
-        test_character = {"Location": [4, 0]}
-        actual = validate_move(board, test_character, direction)
+    def test_validate_move_south_at_southern_edge(self, mock_stdout):
+        mock_board = make_board()
+        mock_character = {"Location": [4, 0]}
+        mock_direction = "S"
+        actual = validate_move(mock_board, mock_character, mock_direction)
         expected = False
         self.assertEqual(expected, actual)
 
-    @patch('maze.game', side_effect=["E"])
-    @patch('maze.make_board')
+
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_validate_move_north_at_northern_edge(self, mock_stdout, board, direction):
-        test_character = {"Location": [0, 4]}
-        actual = validate_move(board, test_character, direction)
+    def test_validate_move_east_at_eastern_edge(self, mock_stdout):
+        mock_board = make_board()
+        mock_character = {"Location": [0, 4]}
+        mock_direction = "E"
+        actual = validate_move(mock_board, mock_character, mock_direction)
         expected = False
         self.assertEqual(expected, actual)
 
-    @patch('maze.game', side_effect=["W"])
-    @patch('maze.make_board')
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_validate_move_north_at_northern_edge(self, mock_stdout, board, direction):
-        test_character = {"Location": [0, 0]}
-        actual = validate_move(board, test_character, direction)
+    def test_validate_move_west_at_western_edge(self, mock_stdout):
+        mock_board = make_board()
+        mock_character = {"Location": [0, 0]}
+        mock_direction = "W"
+        actual = validate_move(mock_board, mock_character, mock_direction)
         expected = False
         self.assertEqual(expected, actual)
 
-    @patch('maze.game', side_effect=["S"])
-    @patch('maze.make_board')
-    def test_validate_move_valid_move(self, board, direction):
-        test_character = {"Location": [0, 0]}
-        actual = validate_move(board, test_character, direction)
+    def test_validate_move_valid_move(self):
+        mock_board = make_board()
+        mock_character = {"Location": [0, 0]}
+        mock_direction = "S"
+        actual = validate_move(mock_board, mock_character, mock_direction)
         expected = True
         self.assertEqual(expected, actual)
